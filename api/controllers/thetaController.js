d753c3c76fa50e4e97590be1200102c5833ca1ea
HUAN CAPITAL <13021427+nguyenvanhuan243@users.noreply.github.com>
2025-02-17 12:05:42 +0700

    add theta token (#4)

---
+++ api/controllers/thetaController.js
@@ -0,0 +1,28 @@
+"use strict";
+const { ethers } = require("ethers");
+const thetaMainnetABI = require("./../../abi/theta/mainnet.json")
+
+module.exports = {
+	tokenInformation: async function (req, res) {
+		try {
+			const web3Provider = new ethers.Contract(
+				process.env.THETA_CONTRACT,
+				thetaMainnetABI,
+				new ethers.getDefaultProvider(process.env.BSC_RPC)
+			);
+			const tokenName = await web3Provider.name();
+			const tokenSymbol = await web3Provider.symbol();
+			const totalSupply = await web3Provider.totalSupply();
+
+			res.json({
+				tokenName: tokenName,
+				tokenSymbol: tokenSymbol,
+				totalSupply: ethers.formatEther(totalSupply),
+				network: 'This is binance smart chain mainnet'
+			});
+		} catch (error) {
+			console.error("Error fetching token data:", error);
+			res.status(500).json({ error: "Failed to retrieve token data" });
+		}
+	}
+}
