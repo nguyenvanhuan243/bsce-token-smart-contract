1f03ac6fb490606bde77c93180721b83465120b2
HUAN CAPITAL <13021427+nguyenvanhuan243@users.noreply.github.com>
2025-02-17 11:33:43 +0700

    Integrate swagger to nodejs application (#1)
    * integrate swagger
    
    * format sc
    
    * Update api docs

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const { swaggerUi, specs } = require('./swagger');
 const app = express();
 
 const bodyParser = require("body-parser")
 require("dotenv").config();
 
 const port = process.env.PORT || 3000;
+// Your routes and other middleware
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
