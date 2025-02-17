2260b3d154b93667dae542736b9b4767b9400aaf
Huan Nguyen <nguyenvanhuan243@gmail.com>
2025-02-17 11:45:23 +0700

    Update coin98 controlller

--- api/routes.js
+++ api/routes.js
@@ -1,9 +1,16 @@
 'use strict';
+
 module.exports = function (app) {
 	const healthServerController = require('./controllers/healthServerController');
 	const smartContractController = require('./controllers/smartContractController');
+	const coin98Controller = require('./controllers/coin98Controller');
+
 	app.route('/')
 		.get(healthServerController.getHealthServer)
+
 	app.route("/token")
 		.get(smartContractController.tokenInformation)
+
+	app.route("/coin98")
+		.get(coin98Controller.tokenInformation)
 };
